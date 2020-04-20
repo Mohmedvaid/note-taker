b93d602237b93b3bc56af6c06e9d0c248ebf2e3f
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-04-19 20:49:34 -0500

    Added unique id using date, added comments

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,9 @@
 // Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
 
-// Sets up the Express App
-// =============================================================
+// setup app and port
 var app = express();
 var PORT = 3000;
 app.use(express.static("public"));
@@ -16,35 +14,22 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-function rndID(finalData) {
-  if(finalData.length ==0){
-    return 1;
-  }
-  var id = Math.floor(Math.random() * (finalData.length + 5))
-  for (var i = 0; i < finalData.length; i++) {
-    if (finalData[i].id == id) {
-      rndID();
-    } else {
-      return id;
-    }
-  }
 
-}
-
-// Basic route that sends the user first to the AJAX Page
+// Homepage route
 app.get("/", function (req, res) {
-  // res.send("Welcome to the Star Wars Page!")
+  //sends index file
   res.sendFile(path.join(__dirname, "index.html"));
 });
-
+//sends notes file
 app.get("/notes.html", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
-
+//sends db.json data
 app.get("/api/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
+//sends specefic id
 app.get("/api/notes/:id", function (req, res) {
   var jsonData;
   fs.readFile("db/db.json", function (err, data) {
@@ -57,28 +42,37 @@ app.get("/api/notes/:id", function (req, res) {
   });
 });
 
-
+//posts data to db.json file
 app.post("/api/notes", function (req, res) {
+  //try catch block
   try {
-    var finalData;
+    //reads db.json file
     fs.readFile("db/db.json", function (err, data) {
+      var finalData;
+      //newEntry points to request received
       var newEntry = req.body;
+      //finalData points to db.json data converted to json format from strings
       finalData = JSON.parse(data);
-      newEntry.id = JSON.stringify(rndID(finalData))
-  
+      //id is added to the data received in the request
+      newEntry.id = JSON.stringify(Date.now())
+
+      //the whole object is pushed into the initail array 
       finalData.push(newEntry);
+      //sends the new array with finalData
       res.send(finalData)
-      JSON.stringify(finalData);
-      if (err) throw err;
+      // //finalData array is stringified
+      // JSON.stringify(finalData);
+
+      //finalData array is stringified and stored in the db.json file
       fs.writeFile("./db/db.json", JSON.stringify(finalData), function (err) {
         if (err) throw err;
         console.log("Saved!");
       });
     });
-    
+
   } catch (error) {
     console.log(error);
-    
+
   }
 });
 
@@ -89,10 +83,10 @@ app.delete("/api/notes/:id", function (req, res) {
 
     fs.readFile("db/db.json", function (err, data) {
       jsonData = JSON.parse(data);
-  
+
       for (var i = 0; i < jsonData.length; i++) {
         if (jsonData[i].id.includes(req.params.id)) {
-          jsonData.splice(i, i+1)
+          jsonData.splice(i, i + 1)
         }
       }
       res.send(jsonData)
@@ -100,7 +94,7 @@ app.delete("/api/notes/:id", function (req, res) {
         if (err) throw err;
         console.log("Delete Saved!");
       });
-      
+
     });
   } catch (error) {
     console.log(error);
